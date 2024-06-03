8b4a63f8c82d5e26fedf1d7e4f2d7e6739b97afd
kkristianr <kristian.ristic@gmail.com>
2024-06-03 15:43:48 +0200

    colors in graphs

--- pages/3 📈 Results.py
+++ pages/3 📈 Results.py
@@ -188,16 +188,6 @@ with st.spinner('Creating the graphs...'):
     for main_term in main_terms:
         main_data = distances_df[distances_df['main term'] == main_term]
         st.text(f" Shift in similarity with respect to: {main_term}")
-        chart = alt.Chart(main_data).mark_line(
-            #point=alt.OverlayMarkDef(filled=False, fill="white")
-        ).encode(
-            x=alt.X('decade:N', title='', axis=alt.Axis(labelAngle=0)),
-            y=alt.Y('distance', title='Cosine similarity'),
-            color='related term',
-            strokeDash='related term',
-            tooltip=['related term', 'distance']  
-        ).interactive()
-
         point = alt.Chart(main_data).mark_point(size=50, filled=True).encode(
             x='decade:N',
             y='distance',
@@ -205,6 +195,17 @@ with st.spinner('Creating the graphs...'):
             tooltip=['related term', 'distance']
         ).interactive()
 
-        st.altair_chart(chart + point, theme='streamlit', use_container_width=True)
+        chart = alt.Chart(main_data).mark_line(
+            #point=alt.OverlayMarkDef(filled=False, fill="white")
+        ).encode(
+            x=alt.X('decade:N', title='', axis=alt.Axis(labelAngle=0)),
+            y=alt.Y('distance', title='Cosine similarity'),
+            color=alt.Color('related term', legend=None),
+            tooltip=['related term', 'distance']  
+        )
+
+
+
+        st.altair_chart((point + chart).resolve_scale(color='independent'), theme='streamlit', use_container_width=True)
 
 
